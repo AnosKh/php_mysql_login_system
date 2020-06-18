b324fe04a8add275c95d606eb293426b5af1d4f5
Kimhab <kimhab.zeecode@gmail.com>
2020-06-18 23:22:23 +0700

    cloud...

---
+++ read.php
@@ -0,0 +1,108 @@
+<?php
+// Check existence of id parameter before processing further
+if(isset($_GET["id"]) && !empty(($_GET["id"]))){
+    // isset Check whether a variable is empty, url: https://www.w3schools.com/php/func_var_isset.asp
+    // $_GET: url: https://www.w3schools.com/PHP/php_superglobals_get.asp 
+    /*
+        PHP $_GET is a PHP super global variable which is used to collect form data after submitting an HTML form with method="get".
+        $_GET can also collect data sent in the URL 
+    */
+
+    /*  trim
+        <?php
+            $str = "Hello World!";
+            echo $str . "<br>";
+            echo trim($str,"Hed!");
+        ?> 
+        output: 
+        Hello World!
+        llo Worl
+     */
+    // Include config file
+    require_once "config.php";
+    
+    // Prepare a select statement
+    $sql = "SELECT * FROM employees WHERE id = ?";
+    
+    if($stmt = mysqli_prepare($link, $sql)){
+        //The prepare() / mysqli_prepare() function is used to prepare an SQL statement for execution
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt, "i", $param_id);
+        
+        // Set parameters
+        $param_id = trim($_GET["id"]);
+        
+        // Attempt to execute the prepared statement
+        if(mysqli_stmt_execute($stmt)){ // Executes a prepared Query
+            $result = mysqli_stmt_get_result($stmt);
+    
+            if(mysqli_num_rows($result) == 1){
+                /* Fetch result row as an associative array. Since the result set contains only one row, we don't need to use while loop */
+                $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                
+                // Retrieve individual field value
+                $name = $row["name"];
+                $address = $row["address"];
+                $salary = $row["salary"];
+            } else{
+                // URL doesn't contain valid id parameter. Redirect to error page
+                header("location: error.php");
+                exit();
+            }
+            
+        } else{
+            echo "Oops! Something went wrong. Please try again later.";
+        }
+    }
+     
+    // Close statement
+    mysqli_stmt_close($stmt);
+    
+    // Close connection
+    mysqli_close($link);
+} else{
+    // URL doesn't contain id parameter. Redirect to error page
+    header("location: error.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>View Record</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h1>View Record</h1>
+                    </div>
+                    <div class="form-group">
+                        <label>Name</label>
+                        <p class="form-control-static"><?php echo $row["name"]; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Address</label>
+                        <p class="form-control-static"><?php echo $row["address"]; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Salary</label>
+                        <p class="form-control-static"><?php echo $row["salary"]; ?></p>
+                    </div>
+                    <p><a href="index.php" class="btn btn-primary">Back</a></p>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
